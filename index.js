ba36b233db5f52b82e3d8f235258ca1004cca801
E. M. Greeff <unmodster@gmail.com>
2022-05-07 15:49:25 -0600

    works

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = new PIXI.Application();
 document.body.appendChild(app.view);
 
 // Create background image
-const background = PIXI.Sprite.from('examples/assets/bg_grass.jpg');
+const background = PIXI.Sprite.from('image.jpg');
 background.width = app.screen.width;
 background.height = app.screen.height;
 app.stage.addChild(background);
@@ -10,14 +10,14 @@ app.stage.addChild(background);
 // Stop application wait for load to finish
 app.stop();
 
-app.loader.add('shader', 'examples/assets/pixi-filters/shader.frag')
+app.loader.add('shader', 'shader.frag')
     .load(onLoaded);
 
 let filter;
 
 // Handle the load completed
 function onLoaded(loader, res) {
-    // Create the new filter, arguments: (vertexShader, fragmentSource)
+    // Create the new filter, arguments: (vertexShader, framentSource)
     filter = new PIXI.Filter(null, res.shader.data, {
         customUniform: 0.0,
     });
